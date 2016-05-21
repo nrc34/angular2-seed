051eb396f69e6fc9469ab589d9ad6fc02af08af7
nrc34 <nuno.ramalho.costa@gmail.com>
2016-05-21 23:16:07 +0100

    enable prod mode

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,7 @@
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {AppComponent} from "./components/Application/Application.component";
+//import {enableProdMode} from "@angular/core";
 
+//enableProdMode();
 
 bootstrap(AppComponent).catch(err => console.error(err));
